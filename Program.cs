74a18e7d91c320858c2205761f54ee7f6ac6b85b
Joe <overtonje@gmail.com>
2020-12-02 14:43:07 -0600

    have a loop to add new crew options

--- Program.cs
+++ Program.cs
@@ -57,61 +57,71 @@ namespace classicHeist
                 Console.WriteLine($"{robber.Name} - Skill Level:{robber.SkillLevel} - Cut: {robber.PercentageCut}%");
 
             });
-
-            Console.WriteLine("Have any new potnetial contacts?");
-            Console.WriteLine("Enter a Name");
-            string newName = Console.ReadLine();
-            Console.WriteLine("What is their specialty?");
-            Console.WriteLine("Hacker (Disables alarms)");
-            Console.WriteLine("Muscle (Disarms guards)");
-            Console.WriteLine("Lock Specialist (Cracks vault)");
-            string newSpecialty;
             while (true)
             {
-                string response = Console.ReadLine();
-                if (response == "Hacker" || response == "Muscle" || response == "Lock Specialist")
+
+                Console.WriteLine("Have any new potential contacts? (y/n)");
+                string YNResponse = Console.ReadLine();
+                // add more logic for answers other than 'n'
+                if (YNResponse == "n")
                 {
-                    newSpecialty = response;
                     break;
                 }
-                else
+                Console.WriteLine("Enter a Name");
+                string newName = Console.ReadLine();
+
+                Console.WriteLine("What is their specialty?");
+                Console.WriteLine("Hacker (Disables alarms)");
+                Console.WriteLine("Muscle (Disarms guards)");
+                Console.WriteLine("Lock Specialist (Cracks vault)");
+                string newSpecialty;
+                while (true)
                 {
-                    Console.WriteLine("enter a specialty listed above");
+                    string response = Console.ReadLine();
+                    if (response == "Hacker" || response == "Muscle" || response == "Lock Specialist")
+                    {
+                        newSpecialty = response;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("enter a specialty listed above");
+                    }
                 }
-            }
-            //could create a try/catch to check for int
-            Console.WriteLine("What is their Skill Level (1-100)");
-            int skillLevel = int.Parse(Console.ReadLine());
-            Console.WriteLine("What percentage cut do they want?");
-            int Cut = int.Parse(Console.ReadLine());
+                //could create a try/catch to check for int
+                Console.WriteLine("What is their Skill Level (1-100)");
+                int skillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("What percentage cut do they want?");
+                int Cut = int.Parse(Console.ReadLine());
 
-            if (newSpecialty == "Hacker")
-            {
-                Hacker NewHacker = new Hacker();
+                if (newSpecialty == "Hacker")
+                {
+                    Hacker NewHacker = new Hacker();
 
-                NewHacker.Name = newName;
-                NewHacker.SkillLevel = skillLevel;
-                NewHacker.PercentageCut = Cut;
-                rolodex.Add(NewHacker);
+                    NewHacker.Name = newName;
+                    NewHacker.SkillLevel = skillLevel;
+                    NewHacker.PercentageCut = Cut;
+                    rolodex.Add(NewHacker);
 
-            }
-            if (newSpecialty == "Muscle")
-            {
-                Muscle NewMuscle = new Muscle();
+                }
+                if (newSpecialty == "Muscle")
+                {
+                    Muscle NewMuscle = new Muscle();
 
-                NewMuscle.Name = newName;
-                NewMuscle.SkillLevel = skillLevel;
-                NewMuscle.PercentageCut = Cut;
-                rolodex.Add(NewMuscle);
-            }
-            if (newSpecialty == "Lock Specialist")
-            {
-                LockSpecialist NewLockSpecialist = new LockSpecialist();
+                    NewMuscle.Name = newName;
+                    NewMuscle.SkillLevel = skillLevel;
+                    NewMuscle.PercentageCut = Cut;
+                    rolodex.Add(NewMuscle);
+                }
+                if (newSpecialty == "Lock Specialist")
+                {
+                    LockSpecialist NewLockSpecialist = new LockSpecialist();
 
-                NewLockSpecialist.Name = newName;
-                NewLockSpecialist.SkillLevel = skillLevel;
-                NewLockSpecialist.PercentageCut = Cut;
-                rolodex.Add(NewLockSpecialist);
+                    NewLockSpecialist.Name = newName;
+                    NewLockSpecialist.SkillLevel = skillLevel;
+                    NewLockSpecialist.PercentageCut = Cut;
+                    rolodex.Add(NewLockSpecialist);
+                }
             }
 
             rolodex.ForEach(robber =>
